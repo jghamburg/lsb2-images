deb535c4261a5f8b7ef9843c347e25decd527d30
Jörg Gellien <joerg.gellien@otto.de>
2018-08-18 21:42:24 +0200

    feat(debug): add debug access for local

--- docker/run.sh
+++ docker/run.sh
@@ -9,10 +9,10 @@ if [ -z ${JAVA_REMOTE_DEBUGGING+x} ]; then
     -server -Xms${java_heap} -Xmx${java_heap} \
     -Dspring.profiles.active=${profile} -jar /app/app.jar
 else
-  echo "starting container, internal debug port is 4242"
+  echo "starting container, internal debug port is ,address=4242,suspend=n"
   java -Djava.security.egd=file:/dev/./urandom \
     -server -Xms${java_heap} -Xmx${java_heap} \
-    -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4242,suspend=n \
+    -Xdebug -Xrunjdwp:server=y,transport=dt_socket,suspend=n \
     -Dspring.profiles.active=${profile} -jar /app/app.jar
 fi
 # The End
